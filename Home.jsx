3edacd6bb83333bf216666b176e2ab8a39b31176
Oli Evans <oli@tableflip.io>
2018-01-21 22:05:15 +0000

    Lab work on l4j.co.uk

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+
+<Box bg='hotpink' p={4} style={{position:'relative', border: 'solid 8px'}}>
+  <Heading align='center' m={0} fontSize={5}>
+    Lucy & Jack
+  </Heading>
+  <TeaLogo my={3} />
+  <Heading align='center' m={0} fontSize={4}>
+    11th August 2018
+  </Heading>
+  
+  <Laugh style={{position:'absolute', bottom: -16, right: -8}} />
+  <Laugh style={{transform: 'scaleX(-1)',position:'absolute', bottom: -16, left: -8}} />
+</Box>
+
+<Text m={5} fontSize={4}>
+  Lucy & Jack are getting <b>married</b>. It's happening on the <b>11th of August, 2018</b> at <Link href="https://goo.gl/maps/h84vFje5PJ82">Château de Pleuville</Link> 
+</Text>
---
+++ Official.jsx
@@ -0,0 +1,107 @@
+<Section align='center' my={6}>
+  <Text>
+    You are invited to the wedding of
+  </Text>
+  <Heading fontSize={5} style={{textTransform:'none'}}>
+    Lucy Ward and Jonathan Frost
+  </Heading>
+  <AccentText fontWeight={500}>
+    2pm on Saturday 11 August 2018
+  </AccentText>
+  <Text mt={3} style={{fontStyle:'italic'}}>
+    Château de Pleuville, France
+  </Text>
+</Section>
+<Section>
+  <Heading>CHATEAU DE PLEUVILLE</Heading>
+  <Text>
+    <p>
+    Chateau de Pleuville is Susan Lowe and Alan Ward's house in the middle of France.
+    </p>
+    <p>
+The Chateau was rebuilt by the Favre d'Echallens family in the 1860s, when it became a fully functioning and independent country house and farm. It remains largely unaltered and the house and grounds are now being renovated by Alan and Susan.
+</p>
+Pleuville is a village in the northern Charente, near the towns of Charroux and Confolens. The nearest big cities are Angoulême, Limoges and Poitiers.
+  </Text>
+  <p>
+    <Arrow mr={1}/>
+    <Link href='http://chateaudepleuville.eu/'>
+      chateaudepleuville.eu
+    </Link>
+  </p>
+</Section>
+
+<Section>
+  <Heading>How to get there</Heading>
+  <AccentText>
+    <div className="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+      <div itemprop="streetAddress">Le Chateau</div>
+      <div itemprop="addressLocality">Pleuville</div>
+      <div itemprop="postalCode">16490</div>
+      <div itemprop="addressRegion">France</div>
+    </div>
+  </AccentText>
+  <Text>
+    <p>
+    Limoges and Poitiers airports are roughly an hour's drive away from Pleuville.
+    </p>
+    <p>
+    Bordeaux and La Rochelle airports are g
+roughly 2 hour's drive away from Pleuville. 
+    </p>
+    <p>
+    Pleuville is about 7 hour's drive from Calais
+    </p>
+  </Text>
+</Section>
+
+<Section>
+  <Heading>
+    Further Details
+  </Heading>
+  <AccentText>
+    Dress code
+  </AccentText>
+  <Text>
+    <p>
+    It is likely to be hot, so the dress code is as smart as you can manage.
+    We will be smart, but wear what you will be comfortable in.
+    </p>
+    <p>
+    Please avoid high heels (or bring a spare pair of suitable shoes) as we will be walking across uneven ground.
+    </p>
+    <AccentText>
+    Children
+    </AccentText>
+    <p>
+    Children are welcome and we will be showing films for them in the evening
+Pleuville is about 7 hour's drive from Calais. to keep them out of trouble.
+    </p>
+  </Text>
+</Section>
+
+<Section>
+  <Heading>Accomodation</Heading>
+  <Text>
+    <p>
+    There is plenty of space for camping and camper vans at the Chateau and you are welcome to stay before and after the wedding. If you plan to camp please let us know.
+    </p>
+    <p>
+    If you prefer a roof over your head, there are lots of lovely local villages and towns in which to stay.
+    </p>
+    <p>
+    The Festival de Confolens happens nearby at this time of year, so it is worth booking accommodation well in advance.
+    </p>
+  </Text>
+</Section>
+
+<Section>
+  <Heading>Gifts</Heading>
+  <Text>
+  We are not expecting any presents. Your presence at the wedding is gift enough.
+  </Text>
+  <Text>
+    If you would like to give something, we will be
+collecting a fund for our children, Alice and Rowan.
+  </Text>
+</Section>
